6b49152a384865de867341c858d5fc32c5543926
kscheltema <k87scheltema@outlook.com>
2021-06-21 21:18:27 +0200

    sending data to firebase and handling the response

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Fragment, useEffect } from "react";
+import { uiActions } from "./store/ui-slice";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
+import Notification from "./components/UI/Notification";
 import Products from "./components/Shop/Products";
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
   useEffect(() => {
-    fetch("https://burgerbuilder-89b34-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT", //PUT overwrites existing data with new data
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending....",
+          message: "Sending cart!",
+        })
+      );
+      const response = await fetch(
+        "https://burgerbuilder-89b34-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT", //PUT overwrites existing data with new data
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data to failed!");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sending cart successful!",
+        })
+      );
+    };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart failed!",
+        })
+      );
     });
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
