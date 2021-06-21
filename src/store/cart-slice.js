e8a26882539808e187fdc658540d2c4509d72dd6
kscheltema <k87scheltema@outlook.com>
2021-06-21 09:00:42 +0200

    notes

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -13,29 +13,31 @@ const cartSlice = createSlice({
       state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          id: newItem.id,
+          //push is allowed redux handles mutation challenges internally
+          id: newItem.id, //new item
           price: newItem,
           quantity: 1,
           totalPrice: newItem.price,
           name: newItem.title,
         });
       } else {
-        existingItem.quantity++;
+        existingItem.quantity++; //if item is existing
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemFormCart(state, action) {
       const id = action.payload;
-      const existingItem = state.items.find((item) => item.id === id);
+      const existingItem = state.items.find((item) => item.id === id); //how many items exists
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
-        state.items = state.items.filter((item) => item.id !== id);
+        state.items = state.items.filter((item) => item.id !== id); //if only 1 items to be removed
       } else {
         existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price; //price update
       }
     },
   },
 });
 
-export const cartActions = cartSlice.actions;
+export const cartActions = cartSlice.actions; //enables dispatch of actions
 export default cartSlice;
