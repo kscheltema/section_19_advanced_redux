68624c4e9bcb2fc595060a2bad85b67e0e063795
kscheltema <k87scheltema@outlook.com>
2021-06-21 19:05:51 +0200

    addItemToCart with redux

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,25 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCT = [
+  { id: "p1", price: 6, title: "my first", description: "book" },
+  { id: "p2", price: 4, title: "my second", description: "book" },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCT.map((product) => (
+          <ProductItem
+            title="Test"
+            price={6}
+            description="This is a first product - amazing!"
+            key={product.id}
+            id={product.id}
+          />
+        ))}
       </ul>
     </section>
   );
