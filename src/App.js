d80cd68f8bd3e99a637b3a5422ba4cb486a9afab
kscheltema <k87scheltema@outlook.com>
2021-06-22 14:15:52 +0200

    useing an action trunk creator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { Fragment, useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
+import { sendCartData } from "./store/cart-slice";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Notification from "./components/UI/Notification";
@@ -15,48 +15,11 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending....",
-          message: "Sending cart!",
-        })
-      );
-      const response = await fetch(
-        "https://burgerbuilder-89b34-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT", //PUT overwrites existing data with new data
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data to failed!");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success",
-          message: "Sending cart successful!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -37,6 +38,52 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    //this is the place to perform side effects
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending....",
+        message: "Sending cart!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://burgerbuilder-89b34-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT", //PUT overwrites existing data with new data
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart failed!");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sending cart successful!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart failed!",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
